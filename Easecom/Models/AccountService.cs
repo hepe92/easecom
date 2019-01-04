9488f092e0bfd84ea201ea63689f955966b54c7f
Slicke <Slicke@DESKTOP-9JMD31U>
2019-01-04 14:07:51 +0100

    ensure created

--- Easecom/Models/AccountService.cs
+++ Easecom/Models/AccountService.cs
@@ -1,4 +1,5 @@
-﻿using Easecom.Models.ViewModels;
+﻿using Easecom.Models.Entities;
+using Easecom.Models.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using System;
@@ -18,18 +19,21 @@ namespace Easecom.Models
         public AccountService(
             IdentityDbContext identityContext,
             UserManager<IdentityUser> userManager,
-            SignInManager<IdentityUser> signInManager
+            SignInManager<IdentityUser> signInManager,
+            EasecomContext context
+
             )
         {
             this.identityContext = identityContext;
             this.userManager = userManager;
             this.signInManager = signInManager;
+            var b = context.Database.EnsureCreated();
         }
 
         public async Task<bool> TryLoginAsync(AccountLoginVM viewModel)
         {
             //// Create DB schema (first time)
-            //var createSchemaResult = await identityContext.Database.EnsureCreatedAsync();
+            var createSchemaResult = await identityContext.Database.EnsureCreatedAsync();
 
             //// Create a hard coded user (first time)
             //var createResult = await userManager.CreateAsync(new IdentityUser("user"), "Password_123");
