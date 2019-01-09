0df7ac7e8a067ceeaac55cc62e940923c3d74ce4
Henrik Pellas <henrik.pellas@hotmail.se>
2019-01-09 15:02:58 +0100

    edit/delete knappar

--- Easecom/Controllers/CaseController.cs
+++ Easecom/Controllers/CaseController.cs
@@ -47,13 +47,19 @@ namespace Easecom.Controllers
 
         }
 
-
-
         [HttpGet]
         [Route("Details")]
         public async Task<IActionResult> Details(int id)
         {
             return View(await service.GetCaseDetailsByIdAsync(id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await service.DeleteCaseByIdAsync(id);
+
+            return RedirectToAction(nameof(Index));
         }
 
     }
