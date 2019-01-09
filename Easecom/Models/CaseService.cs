6715129cbd019da48cb6ba9b012a43ffa92cbf7f
Slicke <Slicke@DESKTOP-9JMD31U>
2019-01-09 18:26:25 +0100

    Edit mm

--- Easecom/Models/CaseService.cs
+++ Easecom/Models/CaseService.cs
@@ -74,25 +74,31 @@ namespace Easecom.Models
             await context.SaveChangesAsync();
         }
 
-        //internal async Task EditCaseAsync(CaseEditVM editCase)
-        //{
-        //    CaseEditVM edit = context.CaseTable.Where(e => e.Id == editCase.Id).FirstOrDefaultAsync;
 
-        //    editCase.Headline
-        //    Description = editCase.Description
-        //    Creator = editCase.Creator
-
-        //    await context.SaveChangesAsync();
-        //}
-
-        public async Task<CaseTable> EditCaseAsync(int id)
+        public async Task<CaseEditVM> EditCaseAsync(CaseEditVM caseEdit)
         {
-            return await context.CaseTable.Select(e => new CaseTable
+            return await context.CaseTable.Select(e => new CaseEditVM
             {
                 Headline = e.Headline,
                 Description = e.Description
             })
-            .SingleOrDefaultAsync(o => o.Id == id);
+            .SingleOrDefaultAsync(o => o.Id == caseEdit.Id);
+        }
+
+        public async Task<CaseEditVM> GetEditDetailsByIdAsync(int id)
+        {
+            var b = await context.
+                CaseTable.
+                Select(o => new CaseEditVM
+                {
+                    Id = o.Id,
+                    Headline = o.Headline,
+                    Description = o.Description,
+                    Creator = o.Creator
+                })
+
+                .SingleOrDefaultAsync(e => e.Id == id);
+            return b;
         }
     }
 }
