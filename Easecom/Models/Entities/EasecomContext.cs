667b04ba7d74295292db1d12352b497321f894ed
Slicke <Slicke@DESKTOP-9JMD31U>
2019-01-10 11:48:41 +0100

    en till

--- Easecom/Models/Entities/EasecomContext.cs
+++ Easecom/Models/Entities/EasecomContext.cs
@@ -29,7 +29,7 @@ namespace Easecom.Models.Entities
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+
                 optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=EasecomDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
             }
         }
