2f0ddd654770320fcff9791d52790a42297457a0
Slicke <Slicke@DESKTOP-9JMD31U>
2019-01-10 17:09:10 +0100

    stylesheet och annat

--- Easecom/Models/ViewModels/CaseDetailsVM.cs
+++ Easecom/Models/ViewModels/CaseDetailsVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,8 +11,11 @@ namespace Easecom.Models.ViewModels
         public CaseFeedItemVM[] FeedItemVMs { get; set; }
 
         public int Id { get; set; }
+        [Required]
         public string Headline { get; set; }
+        [Required]
         public string Description { get; set; }
+
         public string Creator { get; set; }
 
     }
