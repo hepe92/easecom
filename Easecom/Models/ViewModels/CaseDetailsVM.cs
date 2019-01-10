111d3350db0f8299df21e308336d5efaf0dddbb8
Henrik Pellas <henrik.pellas@hotmail.se>
2019-01-10 13:49:47 +0100

    Nestle CaseFeedVM

--- Easecom/Models/ViewModels/CaseDetailsVM.cs
+++ Easecom/Models/ViewModels/CaseDetailsVM.cs
@@ -7,13 +7,21 @@ namespace Easecom.Models.ViewModels
 {
     public class CaseDetailsVM
     {
+        public CaseFeedItemVM[] FeedItemVMs { get; set; }
+
         public int Id { get; set; }
-
         public string Headline { get; set; }
-
         public string Description { get; set; }
-
         public string Creator { get; set; }
 
+    }
+
+    public class CaseFeedItemVM
+    {
+        public int Id { get; set; }
+        public int CaseId { get; set; }
+        public string Message { get; set; }
+        public string Creator { get; set; }
+        public DateTime PostDateTime { get; set; }
     }
 }
